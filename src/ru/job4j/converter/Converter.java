59acd966646e247c27143ffc868bf962e08f2775
AndrewBurUU <andriv80@gmail.com>
2020-06-30 21:07:44 +0800

    0. Что такое тестирование [#312279]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -13,7 +13,7 @@ public class Converter {
 
     public static void main(String[] args) {
 /*
-        int euro = Converter.rubleToEuro(140);
+         int euro = Converter.rubleToEuro(140);
         int dollar=Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euros.");
         System.out.println("140 rubles are " + dollar + " dollars.");
