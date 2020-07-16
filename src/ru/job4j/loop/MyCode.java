956e60c92d9c55a24eb737f4843397e9f61aaf43
AndrewBurUU <andriv80@gmail.com>
2020-07-16 21:11:11 +0800

    5.8. Именование классов и переменных [#312306]

---
+++ src/ru/job4j/loop/MyCode.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class MyCode {
+        public static void main(String[] args) {
+            int js = 10;
+        }
+
+        public static void convert() {
+            String name = "Petr Arsentev";
+            String studentName = "Andrew K";
+        }
+}
