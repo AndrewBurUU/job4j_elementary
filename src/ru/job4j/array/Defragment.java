788cc0e3b5a691333596b838f2f95fd8416da5f0
AndrewBurUU <andriv80@gmail.com>
2020-07-22 21:13:53 +0800

    6.8. Дефрагментация массива [#312236]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int nullIndex = index; /* указатель на null ячейку. */
+                /* переместить первую не null ячейку. Нужен цикл. */
+                for (int notNullIndex = nullIndex + 1; notNullIndex < array.length; notNullIndex++) {
+                    if (array[notNullIndex] != null) {
+                       SwitchArray.swap(array, nullIndex, notNullIndex);
+                       break;
+                    }
+                }
+            }
+//            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+}
