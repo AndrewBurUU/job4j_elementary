f129bfd06b9559fa0bddf44d7460c40649573b61
AndrewBurUU <andriv80@gmail.com>
2020-11-09 22:01:42 +0800

    3. Уникальность элементов и сортировка. [#312178]

--- src/ru/job4j/search/Profiles.java
+++ src/ru/job4j/search/Profiles.java
@@ -7,6 +7,6 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().distinct().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        return profiles.stream().sorted().distinct().map(profile -> profile.getAddress()).collect(Collectors.toList());
     };
 }
--- test/ru/job4j/search/ProfilesTest.java
+++ test/ru/job4j/search/ProfilesTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ProfilesTest {
@@ -25,7 +26,11 @@ public class ProfilesTest {
     @Test
     public void whenSorted() {
         Profiles profilesNew = new Profiles();
-        Predicate<Profile> profile1 = profile -> profile.getAddress();
-        profiles.sort(profile1);
+        List<Address> rsl = profilesNew.collect(profiles);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Moscow", "Lenin str.", 1, 15));
+        expected.add(new Address("Moscow", "Zhukov str.", 102, 154));
+        expected.add(new Address("UU", "Lenin str.", 1,1));
+        assertThat(rsl, is(expected));
     }
 }
