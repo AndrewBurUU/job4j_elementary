49ef98bc6436fb6bc48a01f2d79509a73987644e
AndrewBurUU <andriv80@gmail.com>
2020-12-01 20:50:12 +0800

    EasyStream [#401629]

---
+++ src/ru/job4j/stream/EasyStream.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EasyStream {
+    public static EasyStream of(List<Integer> source) {
+//        throw new UnsupportedOperationException();
+        return new EasyStream(this);
+    }
+
+    public EasyStream map(Function<Integer, Integer> fun) {
+        throw new UnsupportedOperationException();
+    }
+
+    public EasyStream filter(Predicate<Integer> fun) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Integer> collect() {
+        throw new UnsupportedOperationException();
+        return new EasyStream.of(this);
+    }
+}
